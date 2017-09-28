a5ef618c856daf4a93a1a5ec733b3b52f356e7b4
Millesimus <32270710+Millesimus@users.noreply.github.com>
2017-09-28 07:52:37 +0200

    Update database.py
    Fixing a bug, where a "0" was written in the database column "retweet_id" when the original post had already been present in the database.

--- database.py
+++ database.py
@@ -288,7 +288,7 @@ def create_tweet_from_dict(tweet, user=None):
         urls = create_urls_from_entities(tweet["entities"])
         mentions = create_users_from_entities(tweet["entities"])
         # Create new database entry for this tweet
-        t = Tweet.create(
+        t, created = Tweet.get_or_create(
             id=tweet['id'],
             user=user,
             text=tweet['text'],
